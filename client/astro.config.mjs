c7bea73fa844270ec1f28acbbe88a26d7bd41475
Jack <jack@stackupdigital.co.uk>
2024-01-28 00:47:04 +0000

    Made a start on rebuild

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config';
-
-import sanity from "@sanity/astro";
+import { defineConfig } from "astro/config";
+import { sanityIntegration as sanity } from "@sanity/astro";
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity(), react()]
+  integrations: [
+    sanity({
+      projectId: "xwlevetc",
+      dataset: "production",
+      apiVersion: "2024-01-27",
+      useCdn: false,
+    }),
+    react(),
+    tailwind(),
+  ],
 });
---
+++ client/tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Lexend", "sans-serif"],
+    },
+    extend: {
+      maxWidth: {
+        display: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
